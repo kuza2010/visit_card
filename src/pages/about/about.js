bd97758e949f4f030ec59cb89f2aac7620194bab
Artyom <danilinartem1997@gmail.com>
2020-07-06 13:11:09 +0300

    Mock about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,16 +1,15 @@
 import React from "react";
 
+import './index.css'
+
 const About = () => {
     return (
         <div className="col s6 push-s2">
-            <h1> About me</h1>
+            <h4> About me</h4>
             <p>
-                Hi! I’m Stuart, an Assistant Professor at the University of California, San Diego in the Department of
-                Communication and the Halıcıoğlu Data Science Institute. I’m an interpretive social scientist by
-                training with a background in the humanities, but I have just enough expertise in computer science and
-                data science to make trouble. My research, teaching, and broader service are all grounded in the idea
-                that science and technology are inherently social activities, which are directed, operated, and
-                maintained (or not) in certain ways and not others, by people in and between institutions.
+                Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.
+                Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.
+                Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.Hi! This is my mock page.
             </p>
         </div>
     );
