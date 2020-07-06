611dd21c8415fa32999158be4969aa1c154d54d8
Artyom <danilinartem1997@gmail.com>
2020-07-06 16:59:46 +0300

    Add images for skills section

---
+++ src/pages/about/images/techology/code/index.js
@@ -0,0 +1,9 @@
+import android from './android.svg'
+import react from './react.svg'
+import spring from './spring.png'
+
+export default [
+    android,
+    react,
+    spring
+]
---
+++ src/pages/about/images/techology/tool/index.js
@@ -0,0 +1,9 @@
+import gradle from './gradle.svg'
+import git from './git.svg'
+import docker from './docker.svg'
+
+export default [
+    gradle,
+    git,
+    docker
+]
